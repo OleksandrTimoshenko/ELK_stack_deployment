ee3fe9d46c76759205d0873e4b9720a2b6940489
Oleksandr_Timoshenko <oleksandr.tymoshenko@teamvoy.com>
2021-12-03 17:08:59 +0200

    add aliases for staging server setup

--- tasks.py
+++ tasks.py
@@ -13,6 +13,8 @@ def install_vagrant_plugins(c):
 def vagrant_provision(c):
     system(
         "PLAYBOOK_NAME=install_elk.yaml \
+         AWS_ACCESS_KEY=$AWS_ACCESS_KEY \
+         AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
          ENVIRONMENT=vagrant \
 	     vagrant up --provision;"
     )
@@ -23,3 +25,19 @@ def vagrant_destroy(c):
         "PLAYBOOK_NAME=install_elk.yaml \
 	     vagrant destroy -f;"
     )
+
+@task()
+def deploy_server(c):
+    system(
+        "ansible-playbook -v -i $SERVER_IP, --user $SSH_USER --private-key $KEY_RSA install_elk.yaml \
+         --extra-vars \"server_user=$SERVER_USER \
+                        aws_access_key=$AWS_ACCESS_KEY\
+                        aws_secret_access_key=$AWS_SECRET_ACCESS_KEY \
+                        domain_name=$DOMAIN_NAME \
+                        email=$EMAIL \
+                        env_setup=stage\""
+    )
+
+@task
+def ssh_to_server(c):
+    system("ssh -i $KEY_RSA $SSH_USER@$SERVER_IP")
